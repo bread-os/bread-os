f6be703a5dffd0d2de4fc79d52afb19ddb3dfede
himself65 <himself65@outlook.com>
2020-06-03 10:29:27 +0800

    init project

---
+++ src/library.cpp
@@ -0,0 +1,5 @@
+#include "library.h"
+
+void v_main() {
+
+}
---
+++ src/library.h
@@ -0,0 +1,9 @@
+#ifndef BREAD_OS_LIBRARY_H
+#define BREAD_OS_LIBRARY_H
+
+/***
+ * Core
+ */
+void v_main();
+
+#endif //BREAD_OS_LIBRARY_H
