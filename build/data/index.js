79b41419e5853459ac4b8d6d14477492794dffef
himself65 <himself65@outlook.com>
2020-06-03 14:31:04 +0800

    build: update '--build-kernel' option

---
+++ build/data/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  boot: [
+    'src/boot/boot.asm'
+  ]
+}
--- build/scripts/build-kernel.js
+++ build/scripts/build-kernel.js
@@ -1,3 +1,38 @@
-module.exports = () => {
+const { spawn } = require('child_process')
+const { resolve } = require('path')
+const { statSync, mkdirSync, rmdirSync, existsSync } = require('fs')
+const data = require('../data')
 
+module.exports = () => {
+  console.log('build kernel: start')
+  const { boot } = data
+  const outputDir = resolve(process.cwd(), 'out')
+  if (existsSync(outputDir)) {
+    const outputDirStat = statSync(outputDir)
+    if (!outputDirStat.isDirectory()) {
+      if (outputDirStat.isFile()) {
+        // remove 'out' if it's file
+        rmdirSync(outputDir)
+      }
+      mkdirSync(outputDir)
+    }
+  } else {
+    mkdirSync(outputDir)
+  }
+  const cp = spawn(
+    'nasm',
+    [
+      ...boot.map(item => item),
+      '-o',
+      ...boot.map(item => 'out/kernel.bin'),
+      '-l', // todo: user can trigger if generate .lst files
+      ...boot.map(item => 'out/kernel.lst')
+    ],
+    {
+      cwd: process.cwd(),
+      env: process.env
+    }
+  )
+  cp.on('error', console.error)
+  console.log('build kernel: done')
 }
