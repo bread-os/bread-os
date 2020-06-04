e8e1e750d98ea2b05b41f47b50df32be6b4619c3
himself65 <himself65@outlook.com>
2020-06-04 21:10:55 +0800

    build: allowUnknownOption

--- build/index.js
+++ build/index.js
@@ -6,9 +6,9 @@ const { program } = require('commander')
 // make sure this is in node make env
 ok(process.env.NODE_MAKE)
 
-program
+program.allowUnknownOption(true)
   .option('-d --debug', 'turn on debug mode', () => {
-    process.env.DEBUG = true
+    process.env.DEBUG = 'true'
   })
   .option('--build-kernel', 'build kernel', require('./scripts/build-kernel'))
   .option('--clean', 'clean output', require('./scripts/clean'))
