401c0a7ab834a2980224ce13cd28729ebb6d6dd3
himself65 <himself65@outlook.com>
2020-06-13 00:12:46 +0800

    src: init linked-list.h

---
+++ src/include/linked-list.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <bstddef.h>
+
+template <typename T>
+class ListItem
+{
+  ListItem(T value);
+
+  ListItem *prev, *next;
+  // wrapped value
+  T value;
+};
+
+template <typename T>
+class LinkedList
+{
+public:
+  typedef ListItem<T> Item;
+  typedef void travel_callback(Item &);
+  typedef bool compare_callback(const Item &);
+  LinkedList();
+  ~LinkedList();
+
+  size_t length();
+
+  Item *prepend(Item *item);
+  Item *append(Item *item);
+
+  void travel(travel_callback cb);
+  void remove(compare_callback cb);
+
+private:
+  Item *_head, *_tail;
+};
