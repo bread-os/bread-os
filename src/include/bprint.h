f518aadfb071c640e728460774a8281c145d1dc6
himself65 <himself65@outlook.com>
2020-06-12 22:10:34 +0800

    fix: a comment

--- src/include/bprint.h
+++ src/include/bprint.h
@@ -14,7 +14,7 @@ public:
   friend Printer &operator<<(Printer &printer, const char *str);
 
 private:
-  // todo: because we already print to the frist line when bootloader, so we use `pos(80)` here
+  // tip: because we already print to the frist line when bootloader, so we use `pos(80)` here
   Printer() : pos(80) {}
   inline void upperShift();
   inline void nextline();
