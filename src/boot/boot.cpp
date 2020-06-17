30a3ef6cd9868768dd6b74b966d449a34387db72
himself65 <himself65@outlook.com>
2020-06-17 20:49:25 +0800

    src: add headers in boot.cpp

--- src/boot/boot.cpp
+++ src/boot/boot.cpp
@@ -1,11 +1,6 @@
-/* we don't assume stdint.h exists */
-typedef short int int16_t;
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long int uint64_t;
-
+#include <bstddef.h>
 #include <bootboot.h>
+#include <bprint.h>
 
 /* imported virtual addresses, see linker script */
 extern BOOTBOOT bootboot;          // see bootboot.h
