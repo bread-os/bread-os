f40196cf1810a1370bc2404bdd87ad7a4210506d
himself65 <himself65@outlook.com>
2020-06-17 21:00:29 +0800

    src: simplify boot.cpp

--- src/boot/boot.cpp
+++ src/boot/boot.cpp
@@ -2,20 +2,15 @@
 #include <bootboot.h>
 #include <bprint.h>
 
-/* imported virtual addresses, see linker script */
-extern BOOTBOOT bootboot;          // see bootboot.h
-extern unsigned char *environment; // configuration, UTF-8 text key=value pairs
-extern uint8_t fb;                 // linear framebuffer mapped
+extern BOOTBOOT bootboot;
+extern unsigned char *environment;
+extern uint8_t fb;
 
-/******************************************
- * Entry point, called by BOOTBOOT Loader *
- ******************************************/
+// entry point
 void _start()
 {
-  /*** NOTE: this code runs on all cores in parallel ***/
   int x, y, s = bootboot.fb_scanline, w = bootboot.fb_width, h = bootboot.fb_height;
 
-  // cross-hair to see screen dimension detected correctly
   for (y = 0; y < h; y++)
   {
     *((uint32_t *)(&fb + s * y + (w * 2))) = 0x00FFFFFF;
@@ -25,7 +20,6 @@ void _start()
     *((uint32_t *)(&fb + s * (h / 2) + x * 4)) = 0x00FFFFFF;
   }
 
-  // hang for now
   while (1)
     ;
 }
