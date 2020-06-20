3227e9432e8ebe8aa4172055533dcfc372b058a8
himself65 <himself65@outlook.com>
2020-06-20 20:51:55 +0800

    src: update types in 'bstddef.h'

--- src/include/bstddef.h
+++ src/include/bstddef.h
@@ -1,11 +1,14 @@
 #pragma once
 
 typedef unsigned char uint8_t;
+typedef char int8_t;
 typedef short int int16_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
+typedef int int32_t;
 typedef long long unsigned int size_t;
 typedef unsigned long long uint64_t;
+typedef long long int64_t;
 
 // refs: https://wiki.osdev.org/PC_Screen_Font
 typedef struct {
@@ -19,3 +22,5 @@ typedef struct {
   uint32_t width;
   uint8_t glyphs;
 } PSF_font;
+
+#define BREAD_MAGIC 0xB2EAD2EAL
