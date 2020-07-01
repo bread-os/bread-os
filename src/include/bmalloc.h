7cbb7d0f19b83409274c0bc993b8e90acd314315
himself65 <himself65@outlook.com>
2020-07-01 17:47:19 +0800

    src(kernel): add PhysicalMemoryManager

--- src/include/bmalloc.h
+++ src/include/bmalloc.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <bprint.h>
 #include <bstddef.h>
+#include <linked-list.h>
 #include <util.h>
 
 namespace bread_os {
@@ -19,8 +20,41 @@ class MemoryManager {
 
 class PhysicalMemoryPage {
  public:
+  PhysicalMemoryPage() {
+    this->fist_ptr = 0;
+    this->used_size = 0;
+    this->used = false;
+  }
+  explicit PhysicalMemoryPage(size_t used) {
+    this->fist_ptr = 0;
+    this->used_size = 0 + used;
+    this->used = false;
+  };
+
+  // the header of this page
   uint64_t fist_ptr;
-  size_t total_size;
-  PhysicalMemoryPage *next_page;
+  // used size of the page
+  size_t used_size;
+  bool used;
+
+  // aligned with 4KB
+  // see 'EFI_PHYSICAL_ADDRESS EFI_MEMORY_DESCRIPTOR'
+  static constexpr const size_t pageSize = 4096;
+};
+
+class PhysicalMemoryManager {
+ public:
+  static auto &instance() {
+    static PhysicalMemoryManager _pmm;
+    return _pmm;
+  }
+
+  typedef LinkedList<PhysicalMemoryPage> PhysicalLinkedList;
+  PhysicalLinkedList *freePhysicalMemoryPageList;
+  PhysicalLinkedList *usedPhysicalMemoryPageList;
+ private:
+  PhysicalMemoryManager();
+  // deconstruct function should be never called
+  ~PhysicalMemoryManager() = default;
 };
 }  // namespace bread_os
