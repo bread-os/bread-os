6380108a23a25ac5bdcf4a2f00d9ed68b1a4ca33
himself65 <himself65@outlook.com>
2020-10-01 12:36:28 +0800

    fixup!

--- build.rs
+++ build.rs
@@ -8,18 +8,18 @@ fn main() {
     dotenv::dotenv().ok();
 
     println!("start build");
-    // let headers_dir: Vec<_> =
-    //     env::var("INCLUDE_DIR")
-    //         .unwrap_or("".to_string())
-    //         .split(' ')
-    //         .map(|v| {
-    //             let str = format!("-F{}", v);
-    //             str
-    //         })
-    //         .collect();
+    let headers_dir: Vec<_> =
+        env::var("INCLUDE_DIR")
+            .unwrap_or("".to_string())
+            .split(' ')
+            .map(|v| {
+                let str = format!("-F{}", v);
+                str
+            })
+            .collect();
 
     let bindings = bindgen::Builder::default()
-        // .clang_args(headers_dir)
+        .clang_args(headers_dir)
         .header("out/test.h")
         .rust_target(bindgen::RustTarget::Stable_1_40)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
